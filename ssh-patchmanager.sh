818f73e4e4bf19df7b34a0992aacb780b8d159f6
dsondermann <dsondermann@outlook.com>
2024-04-05 15:45:59 +0200

    Update ssh-patchmanager.sh
    manage_ssh_targets can now delete ssh-targets

--- ssh-patchmanager.sh
+++ ssh-patchmanager.sh
@@ -13,48 +13,63 @@ mkdir -p $LOG_DIR
 mkdir -p $OUTPUT_DIR_BASE
 
 manage_ssh_targets() {
-    echo "SSH-Ziele verwalten:"
-    echo "1. Neues SSH-Ziel hinzufügen"
-    echo "2. Bestehendes SSH-Ziel bearbeiten"
-    read -p "Option wählen (1 oder 2): " manage_option
+    echo "Manage SSH Targets:"
+    echo "1. Add a new SSH target"
+    echo "2. Edit an existing SSH target"
+    echo "3. Delete an existing SSH target"
+    read -p "Choose an option (1-3): " manage_option
 
     case $manage_option in
         1)
-            echo "Alias für das neue SSH-Ziel eingeben:"
-            read alias
-            echo "Benutzername für die SSH-Verbindung eingeben:"
-            read user
-            echo "IP-Adresse oder DNS-Name für $alias eingeben:"
-            read address
-            echo "$alias $user $address" >> $SSH_TARGETS_FILE
-            echo "SSH-Ziel $alias wurde hinzugefügt."
+            echo "Enter an alias for the new SSH target:"
+            read -r alias
+            echo "Enter the username for the SSH connection:"
+            read -r user
+            echo "Enter the IP address or DNS name for $alias:"
+            read -r address
+            echo "$alias $user $address" >> "$SSH_TARGETS_FILE"
+            echo "SSH target $alias has been added."
             ;;
         2)
-            echo "Verfügbare SSH-Ziele:"
-            IFS=$'\n' read -d '' -r -a lines < $SSH_TARGETS_FILE
+            echo "Available SSH targets:"
+            IFS=$'\n' read -d '' -r -a lines < "$SSH_TARGETS_FILE"
             for i in "${!lines[@]}"; do
                 echo "$((i+1)). ${lines[i]}"
             done
-            echo "Nummer des zu bearbeitenden Ziels eingeben:"
-            read num
+            echo "Enter the number of the target to edit:"
+            read -r num
             target="${lines[$((num-1))]}"
-            echo "Aktuelle Daten: $target"
-            echo "Neuen Alias für das SSH-Ziel eingeben (aktuell: ${target%% *}):"
-            read new_alias
-            echo "Neuen Benutzer für die SSH-Verbindung eingeben (aktuell: ${target%% * }):"
-            read new_user
-            echo "Neue IP-Adresse oder DNS-Name für $new_alias eingeben (aktuell: ${target##* }):"
-            read new_address
+            echo "Current data: $target"
+            echo "Enter a new alias for the SSH target (current: ${target%% *}):"
+            read -r new_alias
+            echo "Enter a new username for the SSH connection (current: ${target%% * }):"
+            read -r new_user
+            echo "Enter a new IP address or DNS name for $new_alias (current: ${target##* }):"
+            read -r new_address
             lines[$((num-1))]="$new_alias $new_user $new_address"
-            > $SSH_TARGETS_FILE
+            > "$SSH_TARGETS_FILE"
             for line in "${lines[@]}"; do
-                echo "$line" >> $SSH_TARGETS_FILE
+                echo "$line" >> "$SSH_TARGETS_FILE"
             done
-            echo "SSH-Ziel wurde aktualisiert."
+            echo "SSH target has been updated."
+            ;;
+        3)
+            echo "Available SSH targets:"
+            IFS=$'\n' read -d '' -r -a lines < "$SSH_TARGETS_FILE"
+            for i in "${!lines[@]}"; do
+                echo "$((i+1)). ${lines[i]}"
+            done
+            echo "Enter the number of the target to delete:"
+            read -r del_num
+            unset lines[$((del_num-1))]
+            > "$SSH_TARGETS_FILE"
+            for line in "${lines[@]}"; do
+                [[ -n $line ]] && echo "$line" >> "$SSH_TARGETS_FILE"
+            done
+            echo "SSH target has been deleted."
             ;;
         *)
-            echo "Ungültige Option."
-            return
+            echo "Invalid option."
             ;;
     esac
 }
